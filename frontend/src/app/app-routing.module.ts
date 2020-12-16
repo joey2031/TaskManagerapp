4960bf0e4bd2090f10e8b7977b4fd3c740ceed9c
unknown <joey.assal@gmail.com>
2020-12-15 19:15:25 -0500

    Added some things to new list, created new task component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewListComponent } from './components/new-list/new-list.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
 import { TaskViewComponent } from './components/task-view/task-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'lists', pathMatch: 'full' }, // pathMatch: 'full' -> Take full path
   { path: 'new-list', component: NewListComponent },
+  { path: 'new-task', component: NewTaskComponent },
   { path: 'lists', component: TaskViewComponent },
   { path: 'lists/:listId', component: TaskViewComponent }
 ];
--- frontend/src/app/components/new-list/new-list.component.ts
+++ frontend/src/app/components/new-list/new-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-list',
@@ -8,16 +9,17 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class NewListComponent implements OnInit {
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService, private router: Router) { }
 
   ngOnInit(): void {
   }
-   // This gets called when you click create it then calls the
+  // This gets called when you click create it then calls the
   //taskService method which calls the web-requestService method
-  createList(title: string){
-    this.taskService.createList(title).subscribe((response: any) =>{
+  createList(title: string) {
+    this.taskService.createList(title).subscribe((response: any) => {
       console.log(response);
-      // navigate to /lists/responseId
+      // navigate to /lists/response._id
+      this.router.navigate(['/lists', response._id])
     });
   }
 
